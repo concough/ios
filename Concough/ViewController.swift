17917309d910da6865789ba93c5cf094653506a4
abolfazl mac <abolfazl.beh@gmail.com>
2016-11-06 20:33:15 +0330

    Initial Commit

---
+++ Concough/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Concough
+//
+//  Created by Owner on 2016-11-06.
+//  Copyright © 2016 Famba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
